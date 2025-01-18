19c32f43ccf0019b14654d32becee5a6bad28fec
Arumiko <yenchungyu@gmail.com>
2025-01-18 22:03:45 +0900

    test

--- Blox Fruit Trade Scam/Trade.lua
+++ Blox Fruit Trade Scam/Trade.lua
@@ -3,44 +3,41 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Lunar Trade Scam",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Best Trade Scam Script",
+   LoadingTitle = "Rayfield Interface Suite",
    LoadingSubtitle = "by Kurumi",
    Theme = "Ocean", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
+ 
    DisableRayfieldPrompts = false,
    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
+ 
    ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
+       Enabled = true,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "Big Hub"
    },
-
+ 
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+       Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+       Invite = "discord.gg/U66Qpj8bGJ", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
-
-   KeySystem = true, -- Set this to true to use our key system
+ 
+   KeySystem = false, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Kurumi",
+      Title = "Untitled",
       Subtitle = "Key System",
       Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Admin"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
--- *********** 變數 ***********
-local autoAccept = true
 
+ -- 變數
+ local autoAccept = true
 
-
-
--- *********** 函數 ***********
--- 這是一個假設的 function，用於計算交易價值差距
+ -- 函數
  local function calculateTradeValueDifference(trade)
     -- 你的計算邏輯在這裡
     -- 返回交易價值差距的百分比，比如返回 30 表示差距是30%
@@ -67,37 +64,16 @@ game:GetService("Players").PlayerAdded:Connect(function(player)
 end)
 
 
--- *********** 分頁 ***********
- local PlayerTab = Window:CreateTab("Trade", 4483362458) -- Title, Image
- local Section = Tab:CreateSection("Main")
+ -- 分頁
+ local Tab = Window:CreateTab("AutoFarm", 4483362458) -- Title, Image
 
--- 啟用禁用類型按鈕
+ -- 按鈕
  local Toggle = Tab:CreateToggle({
-   Name = "Anti-Jump",
+   Name = "Auto tap",
    CurrentValue = false,
-   Flag = "t", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-   -- The function that takes place when the toggle is pressed
-   -- The variable (Value) is a boolean on whether the toggle is true or false
-   end,
-})
-
- local Toggle = Tab:CreateToggle({
-   Name = "Auto Accept",
-   CurrentValue = false,
-   Flag = "ts", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
       autoAccept = Value -- 更新 autoAccept 的值
-   end,
-})
-
- local Toggle = Tab:CreateToggle({
-   Name = "Freeze Trade",
-   CurrentValue = false,
-   Flag = "tse", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-   -- The function that takes place when the toggle is pressed
-   -- The variable (Value) is a boolean on whether the toggle is true or false
    end,
 })
 
